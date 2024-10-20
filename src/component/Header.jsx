4564031b00675b1c7ac6c942a91ccdd8739828b1
Serizawa Haruto <chha23017@g.nihon-u.ac.jp>
2024-10-21 01:23:10 +0900

    ans1

---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import Navi from "./Navigate"
+export default function Header() {
+    return (
+      <header>
+        <h1>BIRDWATCHING</h1>
+        <img src="\dove.png" alt="a simple dove logo"/>
+        <Navi/>
+        
+      </header>
+    );
+}
